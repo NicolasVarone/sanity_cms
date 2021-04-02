3469f5261b34d9104bd681b178956bd4ee67af4b
nicolas varone <varonenico@gmail.com>
2021-04-02 22:41:20 +0200

    Create home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,20 @@
 import React from "react";
 
+/* Images */
+import GreenTrees from "../images/green_trees.jpg";
+
 const Home = () => {
   return (
     <main>
-      <img src="" alt="" />
-      <section>
-        <h1>Aloha. I'm Nicolas.</h1>
+      <img
+        src={GreenTrees}
+        alt="Green trees"
+        className="absolute object-cover w-full h-full"
+      />
+      <section className="relative flex justify-center min-h-screen pt-36 lg:pt-64 px-8">
+        <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">
+          Aloha. I'm Nicolas.
+        </h1>
       </section>
     </main>
   );
